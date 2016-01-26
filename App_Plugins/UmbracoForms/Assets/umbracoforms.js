ef30611e9fe40e32a6a3204327c13bd896e52031
Umbraco as a Service <support@umbraco.io>
2016-01-26 22:28:23 +0000

    Upgrade files for Forms version 4.1.5

--- App_Plugins/UmbracoForms/Assets/umbracoforms.js
+++ App_Plugins/UmbracoForms/Assets/umbracoforms.js
@@ -31,6 +31,18 @@
         });
 
         $.validator.unobtrusive.adapters.addBool("regex", "umbracoforms_regex");
+
+        $('input[type=submit]').not('.cancel').click(function (evt) {
+            evt.preventDefault();
+            var self = $(this);
+            var frm = self.closest('form');
+            frm.validate();
+            if (frm.valid()) {
+                frm.submit();
+                self.attr('disabled', 'disabled');
+
+            }
+        });
     }
 } (jQuery));
 

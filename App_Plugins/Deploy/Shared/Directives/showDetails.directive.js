44515199c523b9284eaa8dafc0b82efaacd1d7b1
Umbraco as a Service <support@umbraco.io>
2016-02-25 16:11:10 +0000

    Upgrade files for Courier version 2.52.2

---
+++ App_Plugins/Deploy/Shared/Directives/showDetails.directive.js
@@ -0,0 +1,18 @@
+﻿angular.module("umbraco.directives")
+    .directive('showDetails', function () {
+        return {
+            restrict: 'A',
+            scope: {
+                showDetails: '@'
+            },
+            link: function (scope, element, attrs) {
+                element.bind('click', function() {
+                    var target = $(scope.showDetails);
+                    if (target.length > 0) {
+                        $(element).hide();
+                        target.slideDown(350);
+                    }
+                });
+            }
+        };
+    });

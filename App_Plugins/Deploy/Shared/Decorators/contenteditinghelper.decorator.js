68daa399c7640be69607110b88cc813ef746c8d9
Umbraco Cloud <support@umbraco.io>
2017-04-04 06:49:10 +0000

    Upgrade files for Umbraco version 7.5.12

--- App_Plugins/Deploy/Shared/Decorators/contenteditinghelper.decorator.js
+++ App_Plugins/Deploy/Shared/Decorators/contenteditinghelper.decorator.js
@@ -1,36 +1,33 @@
-var contentEditingHelperDecorator = function($delegate, appState, deployService, navigationService, editorState) {
+var contentEditingHelperDecorator = function ($delegate, appState, deployService, navigationService, editorState) {
 
-    $delegate.configureContentEditorButtons = (function() {
+    $delegate.configureContentEditorButtons = (function () {
         var cached_function = $delegate.configureContentEditorButtons;
-        return function() {
+        return function () {
             var buttons = cached_function.apply(this, arguments);
 
             buttons.subButtons.push({
 
-                                letter: "D",
-                                labelKey: "Deploy",
-                                hotKey: "ctrl+d",
-                                handler: function(){
+                letter: "D",
+                labelKey: "Deploy",
+                hotKey: "ctrl+d",
+                handler: function () {
 
-                                                //getting the current tree node to open the dialog against.
-                                                var node = appState.getTreeState("selectedNode");
-                                                if(!node){
-                                                    node = editorState.current;
-                                                }
+                    //getting the current editing node
+                    var node = editorState.current;
 
-                                                navigationService.showDialog({
-                                                    action:{
-                                                        name: "Deploy",
-                                                        metaData:{
-                                                            actionView: "../app_plugins/deploy/views/deploy-dialog.html",
-                                                            dialogMode: true
-                                                        }
-                                                    },                                                    
-                                                    node: node
-                                                });
-                                            }
-                                
-                            });
+                    navigationService.showDialog({
+                        action: {
+                            name: "Deploy",
+                            metaData: {
+                                actionView: "../app_plugins/deploy/views/deploy-dialog.html",
+                                dialogMode: true
+                            }
+                        },
+                        node: node
+                    });
+                }
+
+            });
 
             return buttons;
         };
@@ -40,5 +37,5 @@ var contentEditingHelperDecorator = function($delegate, appState, deployService,
 };
 
 angular.module("umbraco").config(["$provide", function ($provide) {
-    $provide.decorator("contentEditingHelper",  contentEditingHelperDecorator);
+    $provide.decorator("contentEditingHelper", contentEditingHelperDecorator);
 }]);
